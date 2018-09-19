be01b9c25b13cd9b1692f47a2b4ba83f26ccfe22
Margret Gudjonsdottir <margretsg@gmail.com>
2018-09-19 21:56:33 +0000

    Five iterations working

--- tiletraveller_TEST.py
+++ tiletraveller_TEST.py
@@ -1,58 +1,52 @@
 row = 1
 column = 1
-#print("You can travel (N)orth")
-msg = "You can travel:"
-first_direct = "You can travel:"
-#while column < 3:
+direct = "You can travel: "
+base_length = len(direct)
 #Find available directions
-
-#if direction == "n" or "N":
-# tiles that allow travel north
-if row == 1 or (row == 2 and column == 1) or (row == 2 and column == 3)  :
-    msg += "(N)orth"
-    direct = "(N)orth"
-    #if row < 3:
-     #   row += 1                             #ef er farið norður er farið upp um 1 línu (+1)
-#tiles that allow travel south
-if row != 1 and not(row == 3 and column == 2):
-    msg += " or (S)outh"
-    direct = "(S)outh"
-    #if row > 1:
-     #   row -= 1                             #ef er farið suður er farið niður um 1 línu (-1)
-#tiles that allow travel east
-if (row == 2 and column == 1) or (row == 3 and column == 1) or (row == 3 and column == 2):
-    msg += " or (E)ast"
-    direct = "(E)ast"
-    #if column < 3:
-     #   column += 1                          #ef er farið austur er farið til hægri um 1 dálk (+1)
-#tiles that allow travel west
-if (row == 2 and column == 2) or (row == 3 and column == 2) or (row == 3 and column == 3):
-     msg += " or (W)est"
-     direct = "(W)est"
-     # if column > 1:
-     #   column -= 1                          #ef er farið vestur er farið til vinstri um 1 dálk (-1)
-
-#if (row == 1 and column == 3):
-#    print("Victory")
-
-print(msg)
-direction = input("Direction: ")
-print(type(direction))
-concatenate_directions = first_direct + direct
-print(concatenate_directions)
-for i in direct:
-    #fjarlægja sviga utan um fyrsta staf
-if direction == direct[1]:
-    print("direction ok")
-
-print("röð, dálkur fyrir breytingu: ", row, "", column)
-if direction == "N" or direction == "n" and row < 3:
-    row += 1
-elif direction == "S" or direction == "s" and row > 1:
-    row -= 1
-elif direction == "E" or direction == "e" and column < 3:
-    column += 1
-elif direction == "W" or direction == "w" and column > 1:
-    column -= 1
-
-print("röð, dálkur eftir breytingu: ", row, "", column)
+count = 1
+while count < 5:
+    direct = "You can travel: "
+    # tiles that allow travel north
+    if row == 1 or (row == 2 and column == 1) or (row == 2 and column == 3)  :
+        direct += "(N)orth"                          #ef er farið norður er farið upp um 1 línu (+1)
+    #tiles that allow travel south
+    if row != 1 and not(row == 3 and column == 2):
+        if len(direct) > base_length:
+            direct += " or "
+        direct += "(S)outh"                          #ef er farið suður er farið niður um 1 línu (-1)
+    #tiles that allow travel east
+    if (row == 2 and column == 1) or (row == 3 and column == 1) or (row == 3 and column == 2):
+        if len(direct) > base_length:
+            direct += " or "
+        direct += " (E)ast"                           #ef er farið austur er farið til hægri um 1 dálk (+1)
+    #tiles that allow travel west
+    if (row == 2 and column == 2) or (row == 3 and column == 2) or (row == 3 and column == 3):
+        if len(direct) > base_length:
+            direct += " or "
+        direct += " (W)est"                          #ef er farið vestur er farið til vinstri um 1 dálk (-1)
+    print(direct)
+    
+    #concatenate_directions = first_direct + direct
+    #print(concatenate_directions)
+    direction = input("Direction: ")
+    lower_direction = direction.lower()
+    #kanna hvort innslegin stafur(átt) sé í lagi, þ.e. strengur nr 1 í direct (þ.e. stafur nr. 2 sem er þá N, S, W, eða E)
+    #stemmi við innslegin staf. Breyti báðum strengjum í lower svo séu samanburðarhæfir.
+    compareString = "(" + lower_direction + ")"
+    print("CompareString is: ", compareString)
+    if compareString not in direct.lower() :
+        print("Not a valid direction!")
+    else:
+        print("röð, dálkur fyrir breytingu: ", row, "", column)
+        if  lower_direction == "n": #and row < 3:
+            row += 1
+        elif lower_direction == "s": #and row > 1:
+            row -= 1
+        elif lower_direction == "e": #and column < 3:
+            column += 1
+        elif lower_direction == "w": #and column > 1:
+            column -= 1
+    #tæma strenginn svo safnist ekki upp í hverri umferð þær áttir sem notandi má slá inn
+    direct = str()
+    print("röð, dálkur eftir breytingu: ", row, "", column)
+    count += 1
